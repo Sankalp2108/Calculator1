8ca2be538fdcd3dd480b6a8a0209a8201c5cd1b2
Sankalp Mohate <sankalpmohate@gmail.com>
2023-01-10 15:20:37 +0530

    Scientific Calculator

--- scripts.js
+++ scripts.js
@@ -24,3 +24,48 @@ function Clear()
 {
     display.value = "";
 }
+
+function backSpace()
+{
+    display.value = display.value.substring(0,display.value.length-1);
+}
+
+function pow2()
+{
+    display.value = Math.pow(display.value,2);
+}
+
+function pow3()
+{
+    display.value = Math.pow(display.value,3);
+}
+
+function Sin()
+{
+    display.value = Math.sin(display.value);
+}
+
+function Cos()
+{
+    display.value = Math.cos(display.value);
+}
+
+function Tan()
+{
+    display.value = Math.tan(display.value);
+}
+function ln()
+{
+    display.value = Math.log(display.value);
+}
+
+function Fact()
+{
+    var fact = 1;
+    for(var i = 1 ; i <= display.value ; i++)
+    {
+        fact *= i;
+    }
+    display.value = fact;
+}
+
